acde90ef87c92be246a41a1f416b8eaff3a37739
Tommy Ettinger <tommy.ettinger@gmail.com>
2016-02-16 16:24:09 -0800

    Save some space. Casts EVERYWHERE!

--- src/main/java/com/github/tommyettinger/Region.java
+++ src/main/java/com/github/tommyettinger/Region.java
@@ -1,29 +1,30 @@
 package com.github.tommyettinger;
 
-import com.gs.collections.api.list.primitive.*;
+import com.gs.collections.api.PrimitiveIterable;
+import com.gs.collections.api.list.primitive.MutableByteList;
+import com.gs.collections.api.list.primitive.MutableIntList;
+import com.gs.collections.api.list.primitive.MutableLongList;
+import com.gs.collections.api.list.primitive.MutableShortList;
 import com.gs.collections.impl.list.mutable.primitive.ByteArrayList;
-import com.gs.collections.impl.list.mutable.primitive.ShortArrayList;
 import com.gs.collections.impl.list.mutable.primitive.IntArrayList;
 import com.gs.collections.impl.list.mutable.primitive.LongArrayList;
+import com.gs.collections.impl.list.mutable.primitive.ShortArrayList;
 
 /**
  * The primary form of data produced by RegionPacker, meant to be primarily used by that class.
  * Created by Tommy Ettinger on 2/15/2016.
  */
 public class Region {
-    protected MutableByteList byteList;
-    protected MutableShortList shortList;
-    protected MutableIntList intList;
-    protected MutableLongList longList;
+    protected PrimitiveIterable list;
     protected int bytesPer;
     public int size = 0;
-    public static final short MASK8 = 0xff;
-    public static final int MASK16  = 0xffff;
-    public static final long MASK32 = 0xffffffffL;
+    private static final short MASK8 = 0xff;
+    private static final int MASK16  = 0xffff;
+    private static final long MASK32 = 0xffffffffL;
     public Region()
     {
         bytesPer = 8;
-        longList = new LongArrayList(16);
+        list = new LongArrayList(16);
     }
     public Region(int bytesPerItem)
     {
@@ -34,13 +35,13 @@ public class Region {
 
         switch (bytesPer)
         {
-            case 1: byteList = new ByteArrayList(16);
+            case 1: list = new ByteArrayList(16);
                 break;
-            case 2: shortList = new ShortArrayList(16);
+            case 2: list = new ShortArrayList(16);
                 break;
-            case 4: intList = new IntArrayList(16);
+            case 4: list = new IntArrayList(16);
                 break;
-            default: longList = new LongArrayList(16);
+            default: list = new LongArrayList(16);
                 break;
         }
     }
@@ -53,51 +54,80 @@ public class Region {
 
         switch (bytesPer)
         {
-            case 1: byteList = new ByteArrayList(capacity);
+            case 1: list = new ByteArrayList(capacity);
                 break;
-            case 2: shortList = new ShortArrayList(capacity);
+            case 2: list = new ShortArrayList(capacity);
                 break;
-            case 4: intList = new IntArrayList(capacity);
+            case 4: list = new IntArrayList(capacity);
                 break;
-            default: longList = new LongArrayList(capacity);
+            default: list = new LongArrayList(capacity);
                 break;
         }
     }
+
+    public Region(MutableByteList list)
+    {
+        bytesPer = 1;
+        this.list = list;
+        this.size = list.size();
+    }
+
+    public Region(MutableShortList list)
+    {
+        bytesPer = 2;
+        this.list = list;
+        this.size = list.size();
+    }
+
+    public Region(MutableIntList list)
+    {
+        bytesPer = 4;
+        this.list = list;
+        this.size = list.size();
+    }
+
+    public Region(MutableLongList list)
+    {
+        bytesPer = 8;
+        this.list = list;
+        this.size = list.size();
+    }
+
     public Region(long... values)
     {
         bytesPer = 8;
-        longList = new LongArrayList(values);
+        list = new LongArrayList(values);
         size = values.length;
     }
     public Region(byte[] values)
     {
         bytesPer = 1;
-        byteList = new ByteArrayList(values);
+        list = new ByteArrayList(values);
         size = values.length;
     }
     public Region(short[] values)
     {
         bytesPer = 2;
-        shortList = new ShortArrayList(values);
+        list = new ShortArrayList(values);
         size = values.length;
     }
     public Region(int[] values)
     {
         bytesPer = 4;
-        intList = new IntArrayList(values);
+        list = new IntArrayList(values);
         size = values.length;
     }
     public void add(long value)
     {
         switch (bytesPer)
         {
-            case 1: byteList.add((byte)value);
+            case 1: ((MutableByteList)list).add((byte)value);
                 break;
-            case 2: shortList.add((short)value);
+            case 2: ((MutableShortList)list).add((short)value);
                 break;
-            case 4: intList.add((int)value);
+            case 4: ((MutableIntList)list).add((int)value);
                 break;
-            default: longList.add(value);
+            default: ((MutableLongList)list).add(value);
                 break;
         }
         size++;
@@ -107,21 +137,21 @@ public class Region {
         switch (bytesPer)
         {
             case 1:
-                byteList.addAll(values);
+                ((MutableByteList)list).addAll(values);
                 break;
             case 2:
                 for (int i = 0; i < values.length; i++) {
-                    shortList.add((short) (MASK8 & values[i]));
+                    ((MutableShortList)list).add((short) (MASK8 & values[i]));
                 }
                 break;
             case 4:
                 for (int i = 0; i < values.length; i++) {
-                    intList.add(MASK8 & values[i]);
+                    ((MutableIntList)list).add(MASK8 & values[i]);
                 }
                 break;
             default:
                 for (int i = 0; i < values.length; i++) {
-                    longList.add(MASK8 & values[i]);
+                    ((MutableLongList)list).add(MASK8 & values[i]);
                 }
                 break;
         }
@@ -134,20 +164,20 @@ public class Region {
         {
             case 1:
                 for (int i = 0; i < values.length; i++) {
-                    byteList.add((byte) values[i]);
+                    ((MutableByteList)list).add((byte) values[i]);
                 }
                 break;
             case 2:
-                shortList.addAll(values);
+                ((MutableShortList)list).addAll(values);
                 break;
             case 4:
                 for (int i = 0; i < values.length; i++) {
-                    intList.add(MASK16 & values[i]);
+                    ((MutableIntList)list).add(MASK16 & values[i]);
                 }
                 break;
             default:
                 for (int i = 0; i < values.length; i++) {
-                    longList.add(MASK16 & values[i]);
+                    ((MutableLongList)list).add(MASK16 & values[i]);
                 }
                 break;
         }
@@ -160,20 +190,20 @@ public class Region {
         {
             case 1:
                 for (int i = 0; i < values.length; i++) {
-                    byteList.add((byte) values[i]);
+                    ((MutableByteList)list).add((byte) values[i]);
                 }
                 break;
             case 2:
                 for (int i = 0; i < values.length; i++) {
-                    shortList.add((short) values[i]);
+                    ((MutableShortList)list).add((short) values[i]);
                 }
                 break;
             case 4:
-                intList.addAll(values);
+                ((MutableIntList)list).addAll(values);
                 break;
             default:
                 for (int i = 0; i < values.length; i++) {
-                    longList.add(MASK32 & values[i]);
+                    ((MutableLongList)list).add(MASK32 & values[i]);
                 }
                 break;
         }
@@ -186,23 +216,81 @@ public class Region {
         {
             case 1:
                 for (int i = 0; i < values.length; i++) {
-                    byteList.add((byte) values[i]);
+                    ((MutableByteList)list).add((byte) values[i]);
                 }
                 break;
             case 2:
                 for (int i = 0; i < values.length; i++) {
-                    shortList.add((short) values[i]);
+                    ((MutableShortList)list).add((short) values[i]);
                 }
                 break;
             case 4:
                 for (int i = 0; i < values.length; i++) {
-                    intList.add((int) values[i]);
+                    ((MutableIntList)list).add((int) values[i]);
                 }
                 break;
-            default: longList.addAll(values);
+            default: ((MutableLongList)list).addAll(values);
                 break;
         }
         size += values.length;
+    }
+
+    public Region with(long... values)
+    {
+        switch (bytesPer)
+        {
+            case 1:
+                for (int i = 0; i < values.length; i++) {
+                    ((MutableByteList)list).add((byte) values[i]);
+                }
+                break;
+            case 2:
+                for (int i = 0; i < values.length; i++) {
+                    ((MutableShortList)list).add((short) values[i]);
+                }
+                break;
+            case 4:
+                for (int i = 0; i < values.length; i++) {
+                    ((MutableIntList)list).add((int) values[i]);
+                }
+                break;
+            default: ((MutableLongList)list).addAll(values);
+                break;
+        }
+        size += values.length;
+        return this;
+    }
+
+    public Region resetTo(long... values)
+    {
+        bytesPer = 8;
+        list = new LongArrayList(values);
+        size = values.length;
+        return this;
+    }
+
+    public Region resetTo(byte[] values)
+    {
+        bytesPer = 1;
+        list = new ByteArrayList(values);
+        size = values.length;
+        return this;
+    }
+
+    public Region resetTo(short[] values)
+    {
+        bytesPer = 2;
+        list = new ShortArrayList(values);
+        size = values.length;
+        return this;
+    }
+
+    public Region resetTo(int[] values)
+    {
+        bytesPer = 4;
+        list = new IntArrayList(values);
+        size = values.length;
+        return this;
     }
     public void addRange(long start, long end)
     {
@@ -212,22 +300,22 @@ public class Region {
         {
             case 1:
                 for (long i = start; i < end; i++) {
-                    byteList.add((byte) i);
+                    ((MutableByteList)list).add((byte) i);
                 }
                 break;
             case 2:
                 for (long i = start; i < end; i++) {
-                    shortList.add((short) i);
+                    ((MutableShortList)list).add((short) i);
                 }
                 break;
             case 4:
                 for (long i = start; i < end; i++) {
-                    intList.add((int) i);
+                    ((MutableIntList)list).add((int) i);
                 }
                 break;
             default:
                 for (long i = start; i < end; i++) {
-                    longList.add(i);
+                    ((MutableLongList)list).add(i);
                 }
                 break;
         }
@@ -238,10 +326,10 @@ public class Region {
     {
         switch (bytesPer)
         {
-            case 1: return byteList.get(index);
-            case 2: return shortList.get(index);
-            case 4: return intList.get(index);
-            default: return longList.get(index);
+            case 1: return ((MutableByteList)list).get(index);
+            case 2: return ((MutableShortList)list).get(index);
+            case 4: return ((MutableIntList)list).get(index);
+            default: return ((MutableLongList)list).get(index);
         }
     }
     public int getBytesPerItem()
@@ -253,13 +341,13 @@ public class Region {
     {
         switch (bytesPer)
         {
-            case 1: byteList.set(index, (byte)value);
+            case 1: ((MutableByteList)list).set(index, (byte)value);
                 break;
-            case 2: shortList.set(index, (short)value);
+            case 2: ((MutableShortList)list).set(index, (short)value);
                 break;
-            case 4: intList.set(index, (int)value);
+            case 4: ((MutableIntList)list).set(index, (int)value);
                 break;
-            default: longList.set(index, value);
+            default: ((MutableLongList)list).set(index, value);
                 break;
         }
     }
@@ -267,10 +355,10 @@ public class Region {
     {
         switch (bytesPer)
         {
-            case 1: return byteList.toArray();
-            case 2: return Conversion.toBytes(shortList.toArray());
-            case 4: return Conversion.toBytes(intList.toArray());
-            default: return Conversion.toBytes(longList.toArray());
+            case 1: return ((MutableByteList)list).toArray();
+            case 2: return Conversion.toBytes(((MutableShortList)list).toArray());
+            case 4: return Conversion.toBytes(((MutableIntList)list).toArray());
+            default: return Conversion.toBytes(((MutableLongList)list).toArray());
         }
     }
 
@@ -278,10 +366,10 @@ public class Region {
     {
         switch (bytesPer)
         {
-            case 1: return Conversion.toShorts(byteList.toArray());
-            case 2: return shortList.toArray();
-            case 4: return Conversion.toShorts(intList.toArray());
-            default: return Conversion.toShorts(longList.toArray());
+            case 1: return Conversion.toShorts(((MutableByteList)list).toArray());
+            case 2: return ((MutableShortList)list).toArray();
+            case 4: return Conversion.toShorts(((MutableIntList)list).toArray());
+            default: return Conversion.toShorts(((MutableLongList)list).toArray());
         }
     }
 
@@ -289,10 +377,10 @@ public class Region {
     {
         switch (bytesPer)
         {
-            case 1: return Conversion.toInts(byteList.toArray());
-            case 2: return Conversion.toInts(shortList.toArray());
-            case 4: return intList.toArray();
-            default: return Conversion.toInts(longList.toArray());
+            case 1: return Conversion.toInts(((MutableByteList)list).toArray());
+            case 2: return Conversion.toInts(((MutableShortList)list).toArray());
+            case 4: return ((MutableIntList)list).toArray();
+            default: return Conversion.toInts(((MutableLongList)list).toArray());
         }
     }
 
@@ -300,53 +388,25 @@ public class Region {
     {
         switch (bytesPer)
         {
-            case 1: return Conversion.toLongs(byteList.toArray());
-            case 2: return Conversion.toLongs(shortList.toArray());
-            case 4: return Conversion.toLongs(intList.toArray());
-            default: return longList.toArray();
+            case 1: return Conversion.toLongs(((MutableByteList)list).toArray());
+            case 2: return Conversion.toLongs(((MutableShortList)list).toArray());
+            case 4: return Conversion.toLongs(((MutableIntList)list).toArray());
+            default: return ((MutableLongList)list).toArray();
         }
     }
 
     public Region copy()
     {
-        Region next = new Region(bytesPer, size);
-        switch (bytesPer)
-        {
-            case 1: next.byteList = byteList;
-                break;
-            case 2: next.shortList = shortList;
-                break;
-            case 4: next.intList = intList;
-                break;
-            default: next.longList = longList;
+        switch (bytesPer) {
+            case 1:
+                return new Region((MutableByteList) list);
+            case 2:
+                return new Region((MutableShortList) list);
+            case 4:
+                return new Region((MutableIntList) list);
+            default:
+                return new Region((MutableLongList) list);
         }
-        return next;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Region region = (Region) o;
-
-        if (bytesPer != region.bytesPer) return false;
-        if (size != region.size) return false;
-        if (byteList != null ? !byteList.equals(region.byteList) : region.byteList != null) return false;
-        if (shortList != null ? !shortList.equals(region.shortList) : region.shortList != null) return false;
-        if (intList != null ? !intList.equals(region.intList) : region.intList != null) return false;
-        return longList != null ? longList.equals(region.longList) : region.longList == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = byteList != null ? byteList.hashCode() : 0;
-        result = 31 * result + (shortList != null ? shortList.hashCode() : 0);
-        result = 31 * result + (intList != null ? intList.hashCode() : 0);
-        result = 31 * result + (longList != null ? longList.hashCode() : 0);
-        result = 31 * result + bytesPer;
-        result = 31 * result + size;
-        return result;
-    }
 }
